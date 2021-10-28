eee42441cc13279f3cba45982b13822772891c0f
wddaffa <windudaffa99@gmail.com>
2021-10-28 22:18:20 +0700

    Perkenalan

---
+++ Pengenalan.py
@@ -0,0 +1,14 @@
+fullname = input("my name is : ")
+nickname = input("nickname : ")
+nim = input("nim :")
+born = input("born in : ")
+old = input("old : ")
+address = input("address : ")
+phonenumber = input("phone number : ")
+
+print("=============================================")
+print("Assallammualaikum warahmatullohi wa barakhatu")
+
+print("Hi everybody i want to introduce my self")
+print("My name is %s, but you can call me %s, . My nim is %s, i was born in %s,and i am %s, years old im very glad if you want to invite my house in %s,. So, don't forget to call me before with the number %s" % (fullname, nickname, nim, born, old, address, phonenumber))
+input("=============================================")
